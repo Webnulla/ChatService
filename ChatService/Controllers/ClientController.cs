1455343c25576d2ddb203ec87a96dcdf39310e03
Konstantin <muselsps@ya.ru>
2022-11-14 18:45:42 +0300

    Create ClientController.cs

---
+++ ChatService/Controllers/ClientController.cs
@@ -0,0 +1,26 @@
+﻿using ChatService.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+
+        private readonly IMessageService _messageService;
+
+        public ClientController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        [Route("connect")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        public ActionResult<int> Connect(string name)
+        {
+            return Ok(_messageService.AddClient(name));
+        }
+    }
+}
